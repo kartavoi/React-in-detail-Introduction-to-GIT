9884d8a6376a4e2c161474f3f850c3d1e29075fe
kartavoi <afoninm02@gmail.com>
2023-08-30 22:03:41 +0300

    Мой первый коммит проекта

---
+++ main.js
@@ -0,0 +1,29 @@
+
+/*let u70 = document.createElement("h2");
+let u80 = document.createElement("input");
+document.body.append(u70,u80)
+
+u80.addEventListener("keypress", () => {
+       setTimeout(() => {
+              u70.textContent = u80.value;
+       },300)
+})
+
+let butt = document.createElement("button");
+document.body.append(butt)
+butt.style.height = "60px"
+butt.style.width = "60px"
+butt.textContent = "0"
+butt.onclick = add
+
+function add() {
+       butt.innerHTML = +butt.innerHTML + 1
+}
+
+let qvadrat = document.createElement("button")
+document.body.append(qvadrat)
+qvadrat.style.height = "60px"
+qvadrat.style.width = "60px"
+qvadrat.textContent = "qvadrat";*/
+obj = {a: 4, b: {c: 3}};
+console.log(obj.b.c)
